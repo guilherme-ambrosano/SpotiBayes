2e3a7c643df77c1b5af34f2d7ee4df4cf7a132fc
BuildTools <unconfigured@null.spigotmc.org>
2020-11-15 19:16:45 -0300

    mudei as beta pra inflacionadas de zero, mas nao resolveu... coloquei um boxplot pra investigar

--- código/modelo.py
+++ código/modelo.py
@@ -1,16 +1,18 @@
-import pystan
+import numpy as np
 import pandas as pd
-from numpy import median, cov
-from API import API_spotify
 import matplotlib.pyplot as plt
+
+import pystan
+
+from API import API_spotify
 
 
 def preprocess(df):
     df2 = df[["danceability", "energy", "speechiness", # beta
               "liveness", "valence",                   # beta
               "loudness",                              # gama
-              "tempo"# ,                                 # normal
-              # "acousticness", "instrumentalness"     # beta infl zero
+              "tempo",                                 # normal
+              "acousticness", "instrumentalness"       # beta infl zero
               ]]
     df2.loc[:,"loudness"] = -df2.loudness  # invertendo os valores negativos
     return df2
@@ -22,16 +24,27 @@ def preprocess(df):
 
 sapi = API_spotify()
 
-playlist = "cordas dedilhadas"  # FIXME: o modelo só funciona pra algumas playlists
+playlist = "tr00"  # FIXME: o modelo só funciona pra algumas playlists
 sapi.set_playlist(playlist)
 
 dados = preprocess(pd.DataFrame.from_dict(sapi.get_songs_from_playlist()))
 
-# FIXME: mudar todas as betas pra betas inflacionadas de zero...
-#        será que conserta?
+plt.subplots_adjust(hspace=0.5, wspace=0.4)
+for i in range(len(dados.columns)):
+    plt.subplot(3, 3, i+1)
+    var = dados.columns[i]
+    y = dados.loc[:, var]
+    x = np.random.normal(1, 0.04, size=len(y))
+
+    plt.boxplot(y)
+    plt.plot(x, y, "r.", alpha=0.1)
+    plt.title(var)
+plt.show()
+
 # TODO: modelo hierárquico de todas as músicas por playlist
 #       só precisa atualizar o modelo para as músicas recentes
 #       (as playlists não vão mudar)
+
 codigo_stan = """
 data {
     int<lower=0> p;
@@ -39,32 +52,34 @@ data {
     matrix[n,p] musicas;
 }
 parameters {
-    vector<lower=0>[5] alpha0;
-    vector<lower=0>[5] beta0;
+    vector<lower=0, upper=1>[7] theta0;
+    vector<lower=0>[7] alpha0;
+    vector<lower=0>[7] beta0;
     real mu0;
     real<lower=0> sigma0;
     real<lower=0> alpha0_gamma;
     real<lower=0> beta0_gamma;
-    vector<lower=0, upper=1>[2] theta0_zinflbeta;
-    vector<lower=0>[2] alpha0_zinflbeta;
-    vector<lower=0>[2] beta0_zinflbeta;
 }
 model {
     for (i in 1:n)
         for (j in 1:p)
             if (j <= 5)
-                musicas[i, j] ~ beta(alpha0[j], beta0[j]); // transformar todas as betas
-                                                           // em beta inflacionada de zero
+                if (musicas[i,j] < 0.01)
+                    1 ~ bernoulli(theta0[j]);
+                else {
+                    0 ~ bernoulli(theta0[j]);
+                    musicas[i,j] ~ beta(alpha0[j], beta0[j]);
+                }
             else if (j == 6)
                 musicas[i, j] ~ gamma(alpha0_gamma, beta0_gamma);
             else if (j == 7)
                 musicas[i, j] ~ normal(mu0, sigma0);
             else
                 if (musicas[i,j] < 0.01)
-                    1 ~ bernoulli(theta0_zinflbeta[j-7]);
+                    1 ~ bernoulli(theta0[j-2]);
                 else {
-                    0 ~ bernoulli(theta0_zinflbeta[j-7]);
-                    musicas[i,j] ~ beta(alpha0_zinflbeta[j-7], beta0_zinflbeta[j-7]);
+                    0 ~ bernoulli(theta0[j-2]);
+                    musicas[i,j] ~ beta(alpha0[j-2], beta0[j-2]);
                 }
 }
 """
